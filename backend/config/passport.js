16d319215a404185704bdd818308e4b6fcda6f2e
arjunjayan999 <arjunjayan999@gmail.com>
2025-12-07 18:40:32 +0530

    Added RENDER_EXTERNAL_URL support in OAuth

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,13 +2,13 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-
+const BASE_URL = process.env.RENDER_EXTERNAL_URL || process.env.BACKEND_URL;
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
+      callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
